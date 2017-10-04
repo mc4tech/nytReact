3bb4be9cc97a955bc054940568b93e0c914587a7
mc4tech <chavezmeguel@yahoo.com>
2017-10-03 23:36:42 -0400

    nyt api

---
+++ routes/apiRoutes.js
@@ -0,0 +1,20 @@
+const axios = require("axios");
+const router = require("express").Router();
+
+router.get("/recipes", (req, res) => {
+  axios
+    .get({
+    	url: "https://api.nytimes.com/svc/search/v2/articlesearch.json",
+    	qs: {
+    		'api-key': "800615177aef4833ba3d32c6e164e88a",
+    		'q': "nfl",
+		    'begin_date': "20170101",
+		    'end_date': "20171003",
+		    'sort': "newest"
+   			// params: req.query }
+   		}})
+    .then(({ data: { results } }) => res.json(results))
+    .catch(err => res.status(422).json(err));
+});
+
+module.exports = router;
